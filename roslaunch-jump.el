87c46083b062d9dded64243612bae7c83ad682b9
Zheng Qu <quzhengrobot@gmail.com>
2019-04-09 12:36:29 +0200

    refactoring

--- roslaunch-jump.el
+++ roslaunch-jump.el
@@ -6,65 +6,65 @@
 (setq re-pkg "pkg=\"\\([^\"]*\\)\"")
 (setq re-py "type=\"\\([^\"]*\\.py\\)\"")
 
-(defun get-match-from-current-line-- (re)
+(defun roslaunch-jump--get-match-from-current-line (re)
   (let* ((current-line (thing-at-point 'line t))
          (found-match (string-match re current-line)))
     (if found-match
         (match-string 1 current-line))))
 
-(defun remove-new-line-- (str)
-  (replace-in-string-- "\n" "" str))
+(defun roslaunch-jump--remove-new-line (str)
+  (roslaunch-jump--replace-in-string "\n" "" str))
 
-(defun get-pkg-path (pkg-name)
+(defun roslaunch-jump-get-pkg-path (pkg-name)
   (let* ((pkg-path (shell-command-to-string (concat "rospack find " pkg-name)))
          (no-package (string-match-p "\\[rospack\\] Error: package .* not found" pkg-path)))
-    (when (not no-package) (remove-new-line-- pkg-path))))
+    (when (not no-package) (roslaunch-jump--remove-new-line pkg-path))))
 
-(defun get-pkg-file-path (pkg-name file-name)
-  (let* ((pkg-path (get-pkg-path pkg-name))
+(defun roslaunch-jump-get-pkg-file-path (pkg-name file-name)
+  (let* ((pkg-path (roslaunch-jump-get-pkg-path pkg-name))
          (file-path-list (directory-files-recursively pkg-path file-name)))
     (car file-path-list)))
 
-(defun jump-to-py ()
-  (let ((pkg-name (get-match-from-current-line-- re-pkg))
-        (py-file-name (get-match-from-current-line-- re-py)))
+(defun roslaunch-jump-to-py ()
+  (let ((pkg-name (roslaunch-jump--get-match-from-current-line re-pkg))
+        (py-file-name (roslaunch-jump--get-match-from-current-line re-py)))
     (if (and pkg-name py-file-name)
-        (find-file (get-pkg-file-path pkg-name py-file-name)))))
+        (find-file (roslaunch-jump-get-pkg-file-path pkg-name py-file-name)))))
 
-(defun jump-to-path ()
+(defun roslaunch-jump-to-path ()
   (let* ((current-line (thing-at-point 'line t))
          (found-match (string-match re-ros-path current-line)))
     (when found-match
       (let* ((raw-ros-path (match-string 0 current-line))
-             (ros-path (replace-in-string-- "find" "rospack find" raw-ros-path))
+             (ros-path (roslaunch-jump--replace-in-string "find" "rospack find" raw-ros-path))
              (absolute-path (shell-command-to-string (concat "/bin/echo -n " ros-path)))
              (no-package (string-match "\\[rospack\\] Error: package .* not found" absolute-path)))
         (if no-package
             (message (match-string 0 absolute-path))
           (find-file absolute-path))))))
 
-(defun jump-to-pkg (search)
+(defun roslaunch-jump-to-pkg (search)
   (let* (
-         (pkg-name (get-match-from-current-line-- re-pkg))
+         (pkg-name (roslaunch-jump--get-match-from-current-line re-pkg))
          (absolute-path
           (replace-regexp-in-string "\n$" "" (shell-command-to-string (format "rospack find %s" pkg-name)))))
     (if search
         (helm-browse-project-find-files (concat absolute-path "/"))
       (helm-find-files-1 (concat absolute-path "/")))))
 
-(defun jump-to-file ()
+(defun roslaunch-jump-jump-to-file ()
   (interactive)
-  (when (not (jump-to-py)) (jump-to-path)))
+  (when (not (roslaunch-jump-to-py)) (roslaunch-jump-to-path)))
 
-(defun jump-to-pkg-browse-dir ()
+(defun roslaunch-jump-to-pkg-browse-dir ()
   (interactive)
-  (jump-to-pkg nil))
+  (roslaunch-jump-to-pkg nil))
 
-(defun jump-to-pkg-find-files ()
+(defun roslaunch-jump-to-pkg-find-files ()
   (interactive)
-  (jump-to-pkg t))
+  (roslaunch-jump-to-pkg t))
 
-(defun replace-in-string-- (pattern replacement original-text)
+(defun roslaunch-jump--replace-in-string (pattern replacement original-text)
   (replace-regexp-in-string pattern replacement original-text nil 'literal))
 
 (when (fboundp 'nxml-mode)
@@ -73,9 +73,9 @@
     (spacemacs/declare-prefix-for-mode 'nxml-mode "mg" "goto")
     (spacemacs/declare-prefix-for-mode 'nxml-mode "mgp" "ros-package")
     (spacemacs/set-leader-keys-for-major-mode 'nxml-mode "=" 'editorconfig-format-buffer)
-    (spacemacs/set-leader-keys-for-major-mode 'nxml-mode "gg" 'jump-to-file)
-    (spacemacs/set-leader-keys-for-major-mode 'nxml-mode "gpp" 'jump-to-pkg-browse-dir)
-    (spacemacs/set-leader-keys-for-major-mode 'nxml-mode "gpf" 'jump-to-pkg-find-files))
+    (spacemacs/set-leader-keys-for-major-mode 'nxml-mode "gg" 'roslaunch-jump-jump-to-file)
+    (spacemacs/set-leader-keys-for-major-mode 'nxml-mode "gpp" 'roslaunch-jump-to-pkg-browse-dir)
+    (spacemacs/set-leader-keys-for-major-mode 'nxml-mode "gpf" 'roslaunch-jump-to-pkg-find-files))
   (add-to-list 'auto-mode-alist '("\\.launch\\'" . nxml-mode))
   (add-hook 'nxml-mode-hook 'company-mode)
   (add-hook 'nxml-mode-hook 'my-launch-file-config))
@@ -83,36 +83,36 @@
 ;; Path autocompletion for $(find pkg-name)/...
 (require 'company)
 
-(defun prepare-candidates-- (prefix file-list)
+(defun roslaunch-jump--prepare-candidates (prefix file-list)
   (let (new-list)
     (dolist (element file-list new-list)
       (setq new-list (cons (concat prefix element) new-list)))))
 
-(defun get-rospack-absolute-path (rospack-find-str)
-  (let* ((pkg-path (replace-in-string-- "find" "rospack find" rospack-find-str))
+(defun roslaunch-jump--get-rospack-absolute-path (rospack-find-str)
+  (let* ((pkg-path (roslaunch-jump--replace-in-string "find" "rospack find" rospack-find-str))
          (absolute-path (shell-command-to-string (concat "/bin/echo -n " pkg-path)))
          (no-package (string-match "\\[rospack\\] Error: package .* not found" absolute-path)))
     (unless no-package
       absolute-path)))
 
-(defun get-candidates-- (rospack-find-str)
+(defun roslaunch-jump--get-candidates (rospack-find-str)
   (let (absolute-path)
-    (setq absolute-path (get-rospack-absolute-path rospack-find-str))
+    (setq absolute-path (roslaunch-jump--get-rospack-absolute-path rospack-find-str))
     (if (file-directory-p absolute-path)
-        (setq rospack-find-str (replace-in-string-- "/*\\'" "/" rospack-find-str))
-      (setq rospack-find-str (replace-in-string-- "/[^/ \"]+\\'" "/" rospack-find-str)))
-    (prepare-candidates-- rospack-find-str (cdr (cdr (directory-files (get-rospack-absolute-path rospack-find-str)))))))
+        (setq rospack-find-str (roslaunch-jump--replace-in-string "/*\\'" "/" rospack-find-str))
+      (setq rospack-find-str (roslaunch-jump--replace-in-string "/[^/ \"]+\\'" "/" rospack-find-str)))
+    (roslaunch-jump--prepare-candidates rospack-find-str (cdr (cdr (directory-files (roslaunch-jump--get-rospack-absolute-path rospack-find-str)))))))
 
-(defun company-rospack-find-backend (command &optional arg &rest ignored)
+(defun roslaunch-jump-company-rospack-find-backend (command &optional arg &rest ignored)
   (interactive (list 'interactive))
   (case command
-    (interactive (company-begin-backend 'company-rospack-find-backend))
+    (interactive (company-begin-backend 'roslaunch-jump-company-rospack-find-backend))
     (prefix (and (eq major-mode 'nxml-mode)
                  (when (looking-back "\\$(find [^\"\/ )]+)/[^\" ]*")
                    (match-string 0))))
     (candidates
      (remove-if-not
       (lambda (c) (string-prefix-p arg c))
-      (get-candidates-- arg)))))
+      (roslaunch-jump--get-candidates arg)))))
 
-(add-to-list 'company-backends 'company-rospack-find-backend)
+(add-to-list 'company-backends 'roslaunch-jump-company-rospack-find-backend)
